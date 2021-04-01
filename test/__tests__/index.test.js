5b019e2e2c05a4a4fed43465ad4081cf78c2c631
Sergio Cámara Braña <sergio.camara@guidesmiths.com>
2021-04-01 21:53:48 +0200

    test: localstack s3 test bucket

--- test/__tests__/index.test.js
+++ test/__tests__/index.test.js
@@ -1,6 +1,9 @@
+const { CreateBucketCommand, ListObjectsCommand, DeleteObjectCommand } = require("@aws-sdk/client-s3");
+
 const initS3 = require('../../index');
 
 const s3Component = initS3();
+const bucketName = 'test-bucket';
 let s3
 
 describe('Systemic S3 Component Tests', () => {
@@ -16,9 +19,26 @@ describe('Systemic S3 Component Tests', () => {
                 forcePathStyle: true,
             }
         });
+
+        await s3.client.send(new CreateBucketCommand({
+            Bucket: bucketName,
+        }))
     });
 
-    it('should fail', () => {
+    beforeEach(async () => {
+        const res = await s3.client.send(new ListObjectsCommand({
+            Bucket: bucketName,
+        }));
+
+        if (res.Contents) {
+            res.Contents.map((object) => s3.client.send(new DeleteObjectCommand({
+                Bucket: bucketName,
+                Key: object.Key,
+            })))
+        }
+    });
+
+    it('should fail', async () => {
         fail('Test not ready');
     });
 });
