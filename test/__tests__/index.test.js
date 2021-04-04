aaf54eaa64add6520853ebfceb8dd2c9a58b4bdf
Sergio Cámara Braña <sergio.camara@guidesmiths.com>
2021-04-04 17:30:03 +0200

    test: listObject basic test

--- test/__tests__/index.test.js
+++ test/__tests__/index.test.js
@@ -1,10 +1,15 @@
-const { CreateBucketCommand, ListObjectsCommand, DeleteObjectCommand } = require("@aws-sdk/client-s3");
+const {
+    CreateBucketCommand,
+    ListObjectsCommand,
+    DeleteObjectCommand,
+    PutObjectCommand
+} = require("@aws-sdk/client-s3");
 
 const initS3 = require('../../index');
 
 const s3Component = initS3();
 const bucketName = 'test-bucket';
-let s3
+let s3;
 
 describe('Systemic S3 Component Tests', () => {
     beforeAll(async () => {
@@ -22,7 +27,7 @@ describe('Systemic S3 Component Tests', () => {
 
         await s3.client.send(new CreateBucketCommand({
             Bucket: bucketName,
-        }))
+        }));
     });
 
     beforeEach(async () => {
@@ -31,14 +36,33 @@ describe('Systemic S3 Component Tests', () => {
         }));
 
         if (res.Contents) {
-            res.Contents.map((object) => s3.client.send(new DeleteObjectCommand({
-                Bucket: bucketName,
-                Key: object.Key,
-            })))
+            await Promise.all(
+                res.Contents.map((object) => s3.client.send(new DeleteObjectCommand({
+                    Bucket: bucketName,
+                    Key: object.Key,
+                })))
+            );
+
         }
     });
 
-    it('should fail', async () => {
-        fail('Test not ready');
+    it('should list 2 objects stored in the bucket', async () => {
+        await s3.client.send(new PutObjectCommand({
+            Bucket: bucketName,
+            Key: 'example1.txt',
+            Body: 'Example 1 text'
+        }));
+
+        await s3.client.send(new PutObjectCommand({
+            Bucket: bucketName,
+            Key: 'example2.txt',
+            Body: 'Example 2 text'
+        }));
+
+        const res = await s3.listObjects({ bucketName });
+
+        expect(res.Contents).toHaveLength(2);
+        expect(res.Contents[0].Key).toBe('example1.txt');
+        expect(res.Contents[1].Key).toBe('example2.txt');
     });
 });
