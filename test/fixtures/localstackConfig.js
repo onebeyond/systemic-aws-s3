c3955bbbecf287cd58447734c1e190dde8d622f7
Roberto Hernández <roberto.hernandez@guidesmiths.com>
2021-05-04 12:50:44 +0200

    test: add local stack config fixture

---
+++ test/fixtures/localstackConfig.js
@@ -0,0 +1,11 @@
+module.exports = {
+  config: {
+    region: 'us-east-1',
+    endpoint: 'http://localhost:4566',
+    credentials: {
+      secretAccessKey: 'test',
+      accessKeyId: 'test',
+    },
+    forcePathStyle: true,
+  }
+}
